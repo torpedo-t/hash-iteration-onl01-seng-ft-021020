ec0c9320323614e182cff8237be627344f347c2c
Thomas John Helin <thomashelin@yahoo.com>
2020-02-24 21:13:09 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,8 +1,4 @@
-# birthday_kids = {
-#   "Timmy" => 9, 
-#   "Sarah" => 6, 
-#   "Amanda" => 27
-# }
+#birthday part of hash iteration with each
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
