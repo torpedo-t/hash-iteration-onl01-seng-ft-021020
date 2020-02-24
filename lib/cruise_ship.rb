9d0dd0a2617a7e8dc43e2661b32a8034e00afdbb
Thomas John Helin <thomashelin@yahoo.com>
2020-02-24 21:13:38 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,10 +1,4 @@
-# passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
-# suite_e: "Crumpet the Elf"
-# }
+#cruise ship hash iteration with each
 
 def select_winner(passengers)
   winner = ""
